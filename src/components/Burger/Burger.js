b9867ab015c581718ff4eefedca19ad96a05c081
Dimochris <chrisdimo87@gmail.com>
2020-01-13 14:44:00 +0200

    166

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -4,17 +4,24 @@ import classes from "./Burger.css";
 
 const burger = props => {
   //  console.log(props.ingredients);
-  const transformedIngredients1 = Object.values(props.ingredients);
-  const transformedIngredients2 = Object.keys(props.ingredients).map(igKey => {console.log(igKey)});
+  // const transformedIngredients1 = Object.values(props.ingredients);
+  // const transformedIngredients2 = Object.keys(props.ingredients).map(igKey => {console.log(igKey)});
   //console.log("transformedIngredients1:" + transformedIngredients1);
-  console.log("transformedIngredients2:" + transformedIngredients2);
-  const transformedIngredients = Object.keys(props.ingredients).map(igKey => {
+  // console.log("transformedIngredients2:" + transformedIngredients2);
+  let transformedIngredients = Object.keys(props.ingredients).map(igKey => {
     return [...Array(props.ingredients[igKey])].map((_, i) => {
       return <BurgerIngredient key={igKey + i} type={igKey} />;
     });
-  });
-  console.log('aaa:'+Object.keys(props.ingredients));
-  //console.log("transformedIngredients:" + {transformedIngredients});
+  })
+  .reduce((arr, el)=> {
+    return arr.concat(el)
+  },[]);
+  
+  if(transformedIngredients.length === 0 ) {
+    transformedIngredients = <p>Please add ingredients</p>
+  }
+  // console.log('aaa:'+Object.keys(props.ingredients));
+  // console.log("transformedIngredients:" + {transformedIngredients});
 
   return (
     <div className={classes.Burger}>
