39497790cec425762e57e4920ff36c0bd171fca7
Dimochris <chrisdimo87@gmail.com>
2020-04-05 22:07:53 +0300

    practice files ch 9

---
+++ src/containers/Blog/Blog.js
@@ -0,0 +1,57 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+// components
+import Post from '../../components/Post/Post';
+import FullPost from '../../components/FullPost/FullPost';
+import NewPost from '../../components/NewPost/NewPost';
+// classes css
+import './Blog.css';
+
+class Blog extends Component {
+    state = {
+        posts: [],
+        selectedPost: 1
+    }
+
+    componentDidMount() {
+        axios.get('https://jsonplaceholder.typicode.com/posts')
+            .then(response => {
+                const posts = response.data.slice(0, 4);
+                const updatedPosts = posts.map(post => {
+                    return {
+                        ...post,
+                        author: 'Chris'
+                    }
+                })
+                this.setState({ posts: updatedPosts });
+                console.log(response);
+            });
+    }
+
+    clicked = (id) => { 
+        console.log("eimai edw");
+        console.log("ID is: "+ id);
+        console.log(id);
+    }
+
+    render() {
+        const posts = this.state.posts.map(post => {
+            return <Post key={post.id} title={post.title} author='Chris' clicked={()=> this.clicked(post.id)}/>
+        });
+        return (
+            <div>
+                <section className="Posts">
+                    {posts}
+                </section>
+                <section>
+                    <FullPost id={this.state.selectedPost}/>
+                </section>
+                <section>
+                    <NewPost />
+                </section>
+            </div>
+        );
+    }
+}
+
+export default Blog;
